5a0099f4cc578256c2acde982860fc9956ec0fac
CardosoJava <119462996+CardosoJava@users.noreply.github.com>
2022-12-13 21:30:41 -0300

    Adicionei arquivos via upload

---
+++ ProjetoJava/src/br/com/CardosoJava/git/Projeto1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.CardosoJava.git;
+
+/**
+ * @author Matheus Cardoso
+ *
+ */
+public class Projeto1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Integracao com o Github");
+
+	}
+
+}
